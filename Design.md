cf0aae56e479d75d32d1bcc9571ccd0578af2bcc
Calnix <calnix.289@gmail.com>
2024-07-14 14:26:01 +0800

    first draft

---
+++ Design.md
@@ -0,0 +1,14 @@
+## Use of 1/2 instead of bool or 1/0
+
+When you want to store a true/false value, the first instinct is naturally to store it as a boolean. However, because the EVM stores boolean values as a uint8 type, which takes up two bytes, it is actually more expensive to access the value. This is because it EVM words are 32 bytes long, so extra logic is needed to tell the VM to parse a value that is smaller than standard.
+
+The next instinct is to store false as uint256(0), and true as uint256(1). That, it turns out, incurs significant gas costs as well because there is a fixed fee associated with flipping a zero value to something else.
+
+### From rareskills
+
+Most important: avoid zero to one storage writes where possible
+Initializing a storage variable is one of the most expensive operations a contract can do.
+
+When a storage variable goes from zero to non-zero, the user must pay 22,100 gas total (20,000 gas for a zero to non-zero write and 2,100 for a cold storage access).
+
+This is why the Openzeppelin reentrancy guard registers functions as active or not with 1 and 2 rather than 0 and 1. It only costs 5,000 gas to alter a storage variable from non-zero to non-zero.
---
+++ src/ECDSADistributor.sol
@@ -0,0 +1,417 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import "openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
+import "openzeppelin-contracts/contracts/utils/cryptography/EIP712.sol";
+
+import {SafeERC20, IERC20} from "./../lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Ownable2Step, Ownable} from "openzeppelin-contracts/contracts/access/Ownable2Step.sol";
+import {Pausable} from "openzeppelin-contracts/contracts/utils/Pausable.sol";
+import {AccessControl} from "openzeppelin-contracts/contracts/access/AccessControl.sol";
+
+
+//import {ReentrancyGuard} from "@looksrare/contracts-libs/contracts/ReentrancyGuard.sol";
+
+contract ECDSADistributor is EIP712, Pausable, AccessControl, Ownable2Step {
+    using SafeERC20 for IERC20;
+
+    IERC20 internal immutable TOKEN;
+    address internal immutable STORED_SIGNER; //note: if change, should redeploy.
+
+    address public operator;
+    
+    // distribution
+    uint256 public deadline;                // optional: Users can claim until this timestamp
+    uint256 public numberOfRounds;         // num. of rounds
+    uint256 public firstClaimTime;        // startTime of first round   
+    uint256 public lastClaimTime;        // startTime of last round
+
+    // balances
+    uint256 public totalDeposited;
+    uint256 public totalClaimed;
+
+    // emergency state
+    bool public isFrozen;
+
+    struct Claim {
+        address user;
+        uint128 round;
+        uint128 amount;
+    }
+    
+    struct RoundData {
+        uint128 startTime;
+        uint128 allocation;
+        uint128 deposited;
+        uint128 claimed;
+    }
+
+    mapping(uint256 round => RoundData roundData) public allRounds;
+    // 0: false, 1: true.
+    mapping(address user => mapping(uint256 round => uint256 claimed)) public hasClaimed;
+
+// --- errors
+    error DeadlineExceeded();
+    error UserHasClaimed();
+    error InvalidRound();
+    
+    error RoundNotStarted();
+    error RoundNotFinanced();
+    error RoundFullyClaimed();
+    
+    error InvalidSignature();
+    error ECDSAZeroAddress();
+
+    error EmptyArray();
+    error IncorrectLengths();
+
+    error InvalidNewDeadline();
+// ------------------------------------
+
+// --- events
+    event Claimed(address indexed user, uint128 indexed round, uint128 amount);
+    event ClaimedMultiple(address indexed user, uint128[] indexed rounds, uint128 totalAmount);
+
+    event DeadlineUpdated(uint256 newDeadline);
+
+    event SetupRounds(uint256 indexed numOfRounds, uint256 indexed totalAmount, uint256 indexed lastClaimTime);
+    event AddedRounds(uint256 indexed numOfRounds, uint256 indexed totalAmount, uint256 indexed lastClaimTime);
+
+    event Frozen(uint256 indexed timestamp);
+
+
+// ------------------------------------
+
+    constructor(string memory _name, string memory _version, address token, address storedSigner, address owner) EIP712(_name, _version) Ownable(owner) {
+        
+        TOKEN = IERC20(token);
+        STORED_SIGNER = storedSigner;
+    }   
+
+    /*//////////////////////////////////////////////////////////////
+                                 CLAIM
+    //////////////////////////////////////////////////////////////*/
+
+    function claim(uint128 round, uint128 amount, bytes calldata signature) external whenNotPaused {
+
+        // check that deadline as not been exceeded; if deadline has been defined
+        if (deadline > 0) {
+            if (block.timestamp >= deadline) {
+                revert DeadlineExceeded();
+            }
+        }
+
+        // replay attack protection: check that signature has already been used
+        if (hasClaimed[msg.sender][round] == 1) revert UserHasClaimed();
+       
+        // sig.verification
+        _claim(round, amount, signature);
+
+        RoundData memory roundData = allRounds[round];
+        
+        // check if round is legitimate
+        //if (roundData.allocation == 0) revert InvalidRound();
+
+        // sanity checks: round financed, started, not fully claimed
+        if (roundData.deposited == 0) revert RoundNotFinanced();
+        if (roundData.startTime < block.timestamp) revert RoundNotStarted();
+        if (roundData.deposited == roundData.claimed) revert RoundFullyClaimed(); 
+
+        // update round data: increment claimedTokens
+        roundData.claimed += amount;
+
+        // update storage
+        hasClaimed[msg.sender][round] = 1;
+        allRounds[round] = roundData;
+        totalClaimed += amount;
+
+        emit Claimed(msg.sender, round, amount);
+
+        TOKEN.safeTransfer(msg.sender, amount);
+    }
+
+    function claimMultiple(uint128[] calldata rounds, uint128[] calldata amounts, bytes[] calldata signatures) external whenNotPaused {
+
+        // check that deadline as not been exceeded; if deadline has been defined
+        if(deadline > 0) {
+            if (block.timestamp >= deadline) {
+                revert DeadlineExceeded();
+            }
+        }
+        
+        uint256 roundsLength = rounds.length;
+        uint256 amountsLength = amounts.length;
+        uint256 signaturesLength = signatures.length;
+
+        if(roundsLength == amountsLength && roundsLength == signaturesLength) revert IncorrectLengths(); 
+        if(roundsLength == 0) revert EmptyArray(); 
+
+        uint128 totalAmount;
+        for(uint256 i = 0; i < roundsLength; ++i) {
+            
+            // get round no. & round data
+            uint128 round = rounds[i];
+            uint128 amount = amounts[i];
+            bytes memory signature = signatures[i];
+
+            RoundData memory roundData = allRounds[round];
+
+            // replay attack protection: check that signature has already been used
+            if (hasClaimed[msg.sender][round] == 1) revert UserHasClaimed();
+
+            // check if round is legitimate
+            //if (roundData.allocation == 0) revert InvalidRound();
+
+            // sanity checks: round financed, started, not fully claimed
+            if (roundData.deposited == 0) revert RoundNotFinanced();
+            if (roundData.startTime < block.timestamp) revert RoundNotStarted();
+            if (roundData.deposited == roundData.claimed) revert RoundFullyClaimed(); 
+
+            // sig.verification
+            _claim(round, amount, signature);
+        
+            // update round data: increment claimedTokens
+            roundData.claimed += amount;
+            totalAmount += amount;       
+
+            // update storage: signature + roundData
+            hasClaimed[msg.sender][round] = 1;
+            allRounds[round] = roundData;
+        }
+        
+        totalClaimed += totalAmount;
+
+        emit ClaimedMultiple(msg.sender, rounds, totalAmount);
+
+        TOKEN.safeTransfer(msg.sender, totalAmount);
+    }
+
+    function _claim(uint128 round, uint128 amount, bytes memory signature) internal view {
+
+        bytes32 digest = _hashTypedDataV4(keccak256(abi.encode(keccak256("Claim(address user,uint128 round,uint128 amount)"), msg.sender, round, amount)));
+
+        address signer = ECDSA.recover(digest, signature);
+            if(signer != STORED_SIGNER) revert InvalidSignature(); 
+            if(signer == address(0)) revert ECDSAZeroAddress(); // note: is this needed given the earlier
+    }
+
+    // note: only callable once?
+    // note: calldata on L2 
+    // deadline is optional
+    function setupRounds(uint128[] calldata startTimes, uint128[] calldata allocations) external onlyOwner {
+        
+        // can update distribution up till 1 day before old start time
+        if(firstClaimTime > 0){
+            require(block.timestamp < firstClaimTime - 1 days, "Too late");
+        }
+
+        // input validation
+        uint256 startTimesLength = startTimes.length;
+        uint256 allocationsLength = allocations.length;
+        
+        require(startTimesLength > 0, "Empty Array");
+        require(startTimesLength == allocationsLength, "Incorrect Lengths");
+        
+        // update rounds
+        uint256 totalAmount;
+        uint256 prevStartTime = block.timestamp;
+        for(uint256 i = 0; i < startTimesLength; ++i) {         
+
+            uint128 startTime = startTimes[i];
+            uint128 allocation = allocations[i];
+            
+            require(startTime > prevStartTime, "Incorrect startTime");
+            prevStartTime = startTime;
+
+            // update storage 
+            RoundData memory roundData = RoundData({startTime: startTime, allocation: allocation, deposited:0, claimed:0});
+            allRounds[i] = roundData;
+            
+            // increment
+            totalAmount += allocation;
+        }
+
+        // update storage
+        numberOfRounds = startTimesLength;
+        lastClaimTime = startTimes[startTimesLength-1];
+
+        emit SetupRounds(startTimesLength, totalAmount, startTimes[startTimesLength-1]);
+    }
+
+    // only adds extra rounds. must be financed separately
+    function addRounds(uint128[] calldata startTimes, uint128[] calldata allocations) external onlyOwner {
+        require(block.timestamp < deadline, "Deadline exceeded");
+
+        // input validation
+        uint256 startTimesLength = startTimes.length;
+        uint256 allocationsLength = allocations.length;
+
+        require(startTimesLength > 0, "Empty Array");
+        require(startTimesLength == allocationsLength, "Incorrect Lengths");
+
+        // add additional rounds
+        uint256 totalAmount;
+        uint256 numOfRounds = numberOfRounds;
+        uint256 prevStartTime = lastClaimTime;
+        
+        for(uint256 i = numOfRounds; i < (numOfRounds + startTimesLength); ++i) {         
+
+            uint128 startTime = startTimes[i];
+            uint128 allocation = allocations[i];
+
+            require(startTime > prevStartTime, "Incorrect startTime");
+            prevStartTime = startTime;
+
+            // update storage: mapping            
+            RoundData memory roundData = RoundData({startTime: startTime, allocation: allocation, deposited:0, claimed:0});
+            allRounds[i] = roundData;
+            
+            // increment
+            totalAmount += allocation;
+        }
+
+        // update storage
+        numberOfRounds = numOfRounds + startTimesLength;
+        lastClaimTime = startTimes[startTimesLength-1];
+
+        emit AddedRounds(startTimesLength, totalAmount, startTimes[startTimesLength-1]);
+    }
+
+    //note: deadline must be after last claim round
+    function updateDeadline(uint256 newDeadline) external onlyOwner {
+        //if (newDeadline < block.timestamp) revert InvalidNewDeadline(); --- not needed cos of subsequent check
+
+        // allow for 2 week buffer. prevent malicious premature ending
+        uint256 buffer = 14 days;
+        if (newDeadline < lastClaimTime + buffer) revert InvalidNewDeadline();
+
+        deadline = newDeadline;
+        emit DeadlineUpdated(newDeadline);
+    }
+
+
+    /*//////////////////////////////////////////////////////////////
+                                OPERATOR
+    //////////////////////////////////////////////////////////////*/
+
+    // project can fund all rounds at once or partially
+    function deposit(uint256[] calldata rounds) external {
+        require(msg.sender == operator, "Incorrect caller");
+
+        // input validation
+        uint256 roundsLength = rounds.length;
+        if(roundsLength == 0) revert EmptyArray(); 
+
+        // calculate total required for all rounds
+        uint256 totalAmount;
+        for(uint256 i = 0; i < roundsLength; ++i) {
+            
+            // get round no. & round data
+            uint256 round = rounds[i];
+          
+            RoundData storage roundData = allRounds[round];
+            roundData.deposited = roundData.allocation;
+
+            totalAmount += roundData.allocation;
+        }
+
+        // update storage
+        totalDeposited += totalAmount;
+        
+        TOKEN.safeTransferFrom(msg.sender, address(this), totalAmount);
+    }
+
+    function withdraw() external {
+        require(msg.sender == operator, "Incorrect caller");
+        require(block.timestamp > deadline, "Premature withdraw");
+
+        uint256 available = totalDeposited - totalClaimed;
+
+        TOKEN.safeTransfer(msg.sender, available);
+    }
+
+
+    /*//////////////////////////////////////////////////////////////
+                                PAUSABLE
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Pause claim
+     */
+    function pause() external onlyOwner whenNotPaused {
+        _pause();
+    }
+
+    /**
+     * @notice Unpause claim
+     */
+    function unpause() external onlyOwner whenPaused {
+        _unpause();
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                RECOVERY
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Freeze the contract in the event of something untoward occuring
+     * @dev Only callable from a paused state, affirming that distribution should not resume
+     *      Nothing to be updated. Freeze as is.
+            Enables emergencyExit() to be called.
+     */
+    function freeze() external whenPaused onlyOwner {
+        require(isFrozen == false, "Pool is frozen");
+        
+        isFrozen = true;
+
+        emit Frozen(block.timestamp);
+    }  
+
+
+    /**
+     * @notice Recover assets in a black swan event. 
+               Assumed that this contract will no longer be used. 
+     * @dev Transfers all tokens to specified address 
+     * @param receiver Address of beneficiary of transfer
+     */
+    function emergencyExit(address receiver) external whenPaused onlyOwner {
+        require(isFrozen, "Not frozen");
+
+        uint256 balance = TOKEN.balanceOf(address(this));
+
+        TOKEN.safeTransfer(receiver, balance);
+    }
+
+}
+
+
+/**
+
+    Permit
+    - spender presents a signature requesting funds from John's wallet
+    - did John sign the signature? if he did, allow. 
+
+    John signs message off-chain, DApp transmits the signature via txn and handles the asset flow.
+    John pays no gas.
+
+    Similarly in Airdrop,
+
+    - claimer presents a signature: amount, address
+    - did 'we' contract signer, sign said msg?
+
+    Have a specific EOA sign to create all signatures.
+    Store addr of signer on contract
+    Recover signer from signature to verify against on-chain copy.
+
+    If attacker submits spoofed signature, incorrect signer will be returned. 
+    If the correct signature was supplied by the FE, the correct signer will be returned.
+
+*/
+
+/**
+    Attacks
+
+    1. replay attack on other chain/contract:
+        other chain - check mocaToken and hashTypedDataV4
+    2. 
+ */
